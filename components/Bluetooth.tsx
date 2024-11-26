903d661ffdb3dd67a1b70a20bad2ca47562d7e2b
xavieryn <xavier.nishikawa@gmail.com>
2024-11-25 23:08:03 -0800

    Can turn on and off light now :D

--- components/Bluetooth.tsx
+++ components/Bluetooth.tsx
@@ -5,15 +5,19 @@ import { useState } from "react";
 // Define TypeScript interfaces for better type safety
 interface BluetoothDevice extends globalThis.BluetoothDevice {
   gatt?: BluetoothRemoteGATTServer;
+  characteristic?: BluetoothRemoteGATTCharacteristic;
 }
 
 function Bluetooth() {
   const [connected, setIsConnected] = useState(false);
   const [device, setDevice] = useState<BluetoothDevice | null>(null);
   const [isConnecting, setIsConnecting] = useState(false);
-
+  const [characteristic, setCharacteristic] =
+    useState<BluetoothRemoteGATTCharacteristic | null>();
+  const [lightTurnedOn, setLightTurnedOn] = useState(false);
   const connectToDevice = async () => {
     try {
+      // ID needs to be lowercase for some reason
       const SERVICE_UUID = "19B10000-E8F2-537E-4F6C-D104768A1214".toLowerCase();
       const CHARACTERISTIC_UUID =
         "19B10001-E8F2-537E-4F6C-D104768A1214".toLowerCase();
@@ -21,6 +25,7 @@ function Bluetooth() {
       const device = await navigator.bluetooth.requestDevice({
         filters: [{ namePrefix: "Arduino" }, { services: [SERVICE_UUID] }],
       });
+
       console.log("Device selected:", device.name);
 
       if (!device.gatt) {
@@ -32,7 +37,7 @@ function Bluetooth() {
       const characteristic = await service.getCharacteristic(
         CHARACTERISTIC_UUID
       );
-
+      setCharacteristic(characteristic);
       setDevice(device);
       setIsConnected(true);
       device.addEventListener("gattserverdisconnected", onDisconnected);
@@ -41,6 +46,19 @@ function Bluetooth() {
     } finally {
       setIsConnecting(false);
     }
+  };
+
+  const ChangeLightStatus = async () => {
+    console.log("Trying to change light status");
+    let data: Uint8Array;
+    if (lightTurnedOn) {
+      data = new Uint8Array([0x00, 0x00, 0x00]);
+      setLightTurnedOn(false);
+    } else {
+      data = new Uint8Array([0xff, 0xff, 0xff]);
+      setLightTurnedOn(true);
+    }
+    await characteristic?.writeValueWithResponse(data);
   };
 
   const onDisconnected = () => {
@@ -75,6 +93,14 @@ function Bluetooth() {
       ) : (
         <div className="flex flex-col gap-2">
           Connected to: {device?.name || "Unknown Device"}
+          {/* <p>{characteristic?.uuid || "No Characteristic"}</p> */}
+          <Button onClick={ChangeLightStatus}>
+            {!lightTurnedOn ? (
+              <div> Turn on Light </div>
+            ) : (
+              <div>Turn off Light</div>
+            )}
+          </Button>
           <Button onClick={disconnectDevice}>Disconnect</Button>
         </div>
       )}
